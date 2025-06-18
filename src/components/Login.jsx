a905c629fa42155c727a29e98faa07d32062bef6
Sam Turner <sam.turner@kokadjo.net>
2025-06-17 22:05:50 -0400

    submit with simple handling function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,48 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [enteredEmail, SetEnteredEmail] = useState('');
+  const [enteredPassword, SetEnteredPassword] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('Submitted');
+    console.log('Email - ' + enteredEmail)
+    console.log('Password - ' + enteredPassword)
+  }
+
+  function handleEmailChange(event) {
+    SetEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    SetEnteredPassword(event.target.value);
+  }
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" 
+          onChange={handleEmailChange} value={enteredEmail}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" 
+          onChange={handlePasswordChange} value={enteredPassword}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button" >Login</button>
       </p>
     </form>
   );
